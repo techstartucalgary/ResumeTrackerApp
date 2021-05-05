3255f32065d20b1fa7ad1314b81463cb936eb608
armeenrn <67667209+armeenrn@users.noreply.github.com>
2021-05-04 19:11:52 -0600

    Revert Fixed Merge Error

---
+++ BackEnd/quotes/core/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from . models import *
+from rest_framework.response import Response
+from . serializer import *
+import PyPDF2
+from . Parser import *
+import json 
+
+# Create your views here.
+  
+class ReactView(APIView):
+    
+    serializer_class = ReactSerializer
+  
+#     def get(self, request):
+#         detail = [ {"name": detail.name,"detail": detail.detail} 
+#         for detail in React.objects.all()]
+#         return Response(detail)
+  
+  	# Must implement error checking
+    def post(self, request):
+        pdfReader = handleRequest(request)
+        endData = compute(pdfReader)
+        print(endData)
+        return Response(endData)
+
+
+
+#Must implement an exception handling here if file is not valid 
+def handleRequest(request):
+	files = request.FILES
+	f = files['myFile']
+	pdfReader = PyPDF2.PdfFileReader(f)
+	return pdfReader
+	
+	
+	
