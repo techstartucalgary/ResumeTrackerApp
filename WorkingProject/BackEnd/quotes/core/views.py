e5754e643faf750368497fd5388163fa360305a9
armeenrn <67667209+armeenrn@users.noreply.github.com>
2021-05-04 19:13:49 -0600

    Revert

--- WorkingProject/BackEnd/quotes/core/views.py
+++ WorkingProject/BackEnd/quotes/core/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.shortcuts import render
 from rest_framework.views import APIView
 from . models import *
@@ -40,5 +39,3 @@ def handleRequest(request):
 	
 	
 	
-=======
->>>>>>> parent of a0fe33c1 (Merge branch 'main' into frontend-fix)
